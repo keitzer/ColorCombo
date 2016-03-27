26e528bbd2b7cc68792ea59a01dff63be77ae841
keitzer <ogorek@usc.edu>
2016-03-27 10:28:33 -0400

    red: cannot unlock panel test

--- ColorComboTests/SecurityPanelSpec.m
+++ ColorComboTests/SecurityPanelSpec.m
@@ -22,6 +22,18 @@ describe(@"Security Panel", ^{
 			[[returnedOrder should] equal:@"blue,green"];
 		});
 	});
+	
+	context(@"when passed a marker color and 2 chips", ^{
+		context(@"that don't connect", ^{
+			it(@"says it cannot be unlocked", ^{
+				SecurityPanel *panel = [[SecurityPanel alloc] init];
+				
+				NSArray *chipsArray = @[@"blue,green", @"blue,green", @"blue,red"];
+				NSString *returnedOrder = [panel unlockPanelWithChipsAndMarkers:chipsArray];
+				[[returnedOrder should] equal:@"Cannot unlock master panel"];
+			});
+		});
+	});
 });
 
 SPEC_END
