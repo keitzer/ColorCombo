7ed055dd480214580c252e8199dd499a87f60902
keitzer <ogorek@usc.edu>
2016-03-27 09:54:34 -0400

    Initial Commit

---
+++ ColorCombo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ColorCombo
+//
+//  Created by Alex Ogorek on 3/27/16.
+//  Copyright © 2016 pillartest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ColorCombo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ColorCombo
+//
+//  Created by Alex Ogorek on 3/27/16.
+//  Copyright © 2016 pillartest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
